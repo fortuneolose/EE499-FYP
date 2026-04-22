dc5205b5ded524f503ada7d9a350a3f00d010a5a
FORTUNE EGHA OLOSE <fortune.olose.2022@mumail.ie>
2026-04-22 16:37:05 +0100

    Auto-sync from EECR-HN3V674 at 2026-04-22 16:37:02

--- start_sync.bat
+++ start_sync.bat
@@ -1,5 +1,5 @@
 @echo off
 title EE499-FYP Auto-Sync
 echo Starting EE499-FYP file watcher...
-"C:\Program Files\Git\bin\bash.exe" "%~dp0auto_sync.sh" 60
+"C:\Users\21455956\AppData\Local\Programs\Git\bin\bash.exe" "%~dp0auto_sync.sh" 60
 pause
