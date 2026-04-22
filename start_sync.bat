19fb7c4ee1d7bdc221e555289e1f6e91a011d44d
FortuneMU2025 <fortune.olose.2022@mumail.ie>
2026-04-23 00:30:17 +0100

    Auto-sync from Fortune at 2026-04-23 00:30:17

--- start_sync.bat
+++ start_sync.bat
@@ -1,5 +1,18 @@
 @echo off
 title EE499-FYP Auto-Sync
 echo Starting EE499-FYP file watcher...
-"C:\Users\21455956\AppData\Local\Programs\Git\bin\bash.exe" "%~dp0auto_sync.sh" 60
+
+set "BASH_EXE="
+if exist "%ProgramFiles%\Git\bin\bash.exe" set "BASH_EXE=%ProgramFiles%\Git\bin\bash.exe"
+if not defined BASH_EXE if exist "%ProgramFiles(x86)%\Git\bin\bash.exe" set "BASH_EXE=%ProgramFiles(x86)%\Git\bin\bash.exe"
+if not defined BASH_EXE if exist "%LocalAppData%\Programs\Git\bin\bash.exe" set "BASH_EXE=%LocalAppData%\Programs\Git\bin\bash.exe"
+if not defined BASH_EXE if exist "C:\Users\21455956\AppData\Local\Programs\Git\bin\bash.exe" set "BASH_EXE=C:\Users\21455956\AppData\Local\Programs\Git\bin\bash.exe"
+
+if not defined BASH_EXE (
+    echo ERROR: Git Bash not found. Install Git for Windows from https://git-scm.com/download/win
+    pause
+    exit /b 1
+)
+
+"%BASH_EXE%" "%~dp0auto_sync.sh" 60
 pause
